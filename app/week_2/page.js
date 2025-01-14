fb98575d9c7a48d28e3e4a223a28de20324b776f
Dafisto <166645358+Dafisto@users.noreply.github.com>
2025-01-14 10:28:34 -0700

    commit week 2

---
+++ app/week_2/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to week 2</h1>
+        <p><Link href="/">Go to Home</Link></p>
+      </div>
+    );
+  }
